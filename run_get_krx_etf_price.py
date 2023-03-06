f372f51718c3c254d485f32d3a9470604752aef4
Junho <pinedance@gmail.com>
2023-03-06 23:13:43 +0900

    update github action

--- run_get_krx_etf_price.py
+++ run_get_krx_etf_price.py
@@ -55,18 +55,19 @@ with open(rst_path, "w", encoding="utf-8") as fl:
     fl.write(html_table)
 
 ############################################################################
-#%%
+# %%
 tickers = [
     "148070", "195970", "195980", "203780", "218420", "251350", "273130", "276000", "278530", "283580",
     "305080", "329750", "332620", "357870", "371160", "371470", "379800", "379810", "385560", "390390", "394660", "394670", "396510", "399580",
     "411060", "414270", "414780", "418670", "419420", "419430", "430500", "437080", "438330", "448300"
 ]
-tickersA = [ "A{}".format(t) for t in tickers ]
-#%%
-etf_data_selected = etf_data[ tickersA ]
+
+tickersA = ["A{}".format(t) for t in tickers]
+# %%
+etf_data_selected = etf_data[tickersA]
 html_table_selected = etf_data_selected.to_html()
 
-#%%
+# %%
 rst_path2 = path.join("dist", "KRX", "etf-price-selected.html")
 with open(rst_path2, "w", encoding="utf-8") as fl:
     fl.write(html_table_selected)
