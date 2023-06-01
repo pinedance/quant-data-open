68f69acd6436fc4fa2b9b4d15f0cadee02fa6e65
Junho <pinedance@gmail.com>
2023-06-01 16:17:12 +0900

    add ecos daily data

---
+++ run_get_ecos_data_daily.py
@@ -0,0 +1,63 @@
+# %%
+import os
+import datetime as dt
+import numpy as np
+import pandas as pd
+from PublicDataReader import Ecos
+from cons import ecos_search_codes_daily as ecos_search_codes
+from tools import yyyymm2quarter
+from dotenv import load_dotenv
+
+# %%
+# PyblicDataReader : https://github.com/WooilJeong/PublicDataReader
+load_dotenv()
+service_key = os.getenv("ECOS_KEY")
+if service_key is None:
+    raise ValueError("!!! There is no Service Key of ECOS API !!!")
+
+api = Ecos(service_key)
+print("ECOS API module is Ready.")
+
+# %%
+date_delta_f = dt.timedelta(days=5)
+date_delta_b = dt.timedelta(days=400)
+search_range = dict()
+search_range['close_D'] = (dt.datetime.today() + date_delta_f).strftime('%Y%m%d')
+search_range['open_D'] = (dt.datetime.today() - date_delta_b).strftime('%Y%m%d')
+
+print("Search Range:", search_range)
+
+# %%
+search_opt = dict(
+    D=dict(
+        주기="D", 검색시작일자=search_range["open_D"], 검색종료일자=search_range["close_D"]
+    ),
+)
+
+select_cols = ["시점", "값"]
+
+# %%
+print(ecos_search_codes)
+_data_D = dict()
+
+# %%
+for dc in ecos_search_codes:
+    data_name = dc["이름"]
+    _search_opt = search_opt["D"]
+    _data_rp = _data_D
+    _df = api.get_statistic_search(**dc["코드"], **_search_opt)
+    _data_rp[data_name] = _df[select_cols].set_index(select_cols[0])
+    print("Data Downloaded:", data_name)
+
+# %%
+df_D = pd.concat(list(_data_D.values()), axis=1)
+df_D.columns = list(_data_D.keys())
+df_D = df_D.astype('float64')
+
+
+# %%
+html_table = df_D.to_html(na_rep='')
+rst_path = os.path.join("dist", "ECOS", "economic-data-daily.html")
+with open(rst_path, "w", encoding="utf-8") as fl:
+    fl.write(html_table)
+
