edb08105b6d3dc35d701622a1f806819a79a783d
Junho <pinedance@gmail.com>
2023-06-02 15:22:51 +0900

    add run_get_nv_data_daily

--- python_modules/tools.py
+++ python_modules/tools.py
@@ -1,11 +1,20 @@
+import requests
+
 month2quarter_dict = {
     '01': 'Q1', '02': 'Q1', '03': 'Q1', '04': 'Q2', '05': 'Q2', '06': 'Q2',
     '07': 'Q3', '08': 'Q3', '09': 'Q3', '10': 'Q4', '11': 'Q4', '12': 'Q4'
 }
-
 
 def yyyymm2quarter(yyyymm, q_dict=month2quarter_dict):
     y = yyyymm[:4]
     m = yyyymm[4:]
     q = q_dict[m]
     return f"{y}{q}"
+
+def get_json(url, keys=["result"]):
+    d = requests.get(url)
+    rst = d.json()
+    if (keys is not None) and (len(keys) > 0):
+        for k in keys:
+            rst = rst[k]
+    return rst
