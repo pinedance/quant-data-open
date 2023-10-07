281fbfce66fe004e340c84a64c3648a08104ef2e
Junho <pinedance@gmail.com>
2023-10-07 10:35:23 +0900

    add new script

--- run_get_yh_data_daily.py
+++ run_get_yh_data_daily.py
@@ -6,7 +6,7 @@ from os import path
 import pandas as pd
 from tqdm import tqdm
 import FinanceDataReader as fdr
-from cons import config_gsheet_tickers_req_yh as config_tickers_req
+from cons import config_gsheet_tickers_req_yh2 as config_tickers_req
 from cons import delta_days
 
 # %%
@@ -42,10 +42,10 @@ etf_data.columns = ["{}".format(ticker) for ticker in etf_tickers]
 etf_data = etf_data.astype('float64')
 
 # %%
-html_table = etf_data.to_html(na_rep='')
-# html_table = etf_data.dropna(axis=0).to_html(na_rep='')
+# html_table = etf_data.to_html(na_rep='')
+html_table = etf_data.dropna(axis=0).to_html(na_rep='')
 
 # %%
-rst_path = path.join("dist", "YH", "etf-adjusted-price-selected.html")
+rst_path = path.join("dist", "YH", "economic-data-daily.html")
 with open(rst_path, "w", encoding="utf-8") as fl:
     fl.write(html_table)
