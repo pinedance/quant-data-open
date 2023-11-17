741c34d47ba3e38ac93c19794fb06fa88e5d2f90
Junho <pinedance@gmail.com>
2023-11-17 13:49:15 +0900

    fix due to github action error

--- python_modules/tools.py
+++ python_modules/tools.py
@@ -1,9 +1,11 @@
 import requests
+import FinanceDataReader as fdr
 
 month2quarter_dict = {
     '01': 'Q1', '02': 'Q1', '03': 'Q1', '04': 'Q2', '05': 'Q2', '06': 'Q2',
     '07': 'Q3', '08': 'Q3', '09': 'Q3', '10': 'Q4', '11': 'Q4', '12': 'Q4'
 }
+
 
 def yyyymm2quarter(yyyymm, q_dict=month2quarter_dict):
     y = yyyymm[:4]
@@ -11,10 +13,17 @@ def yyyymm2quarter(yyyymm, q_dict=month2quarter_dict):
     q = q_dict[m]
     return f"{y}{q}"
 
+
 def get_json(url, keys=["result"]):
     d = requests.get(url)
     rst = d.json()
     if (keys is not None) and (len(keys) > 0):
         for k in keys:
             rst = rst[k]
+    return rst
+
+
+def fin_data(*arg):
+    d = fdr.DataReader(*arg)
+    rst = d['Adj Close'] if 'Adj Close' in d.index else d['Close']
     return rst
