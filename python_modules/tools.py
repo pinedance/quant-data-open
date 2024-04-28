783c7e1e3b8ff2faf129a6b1caeda29911051a97
Junho <pinedance@gmail.com>
2024-04-28 16:43:31 +0900

    update tools.fin_data 국내 etf 데이터 yahoo로부터 먼저 받아오기

--- python_modules/tools.py
+++ python_modules/tools.py
@@ -1,3 +1,5 @@
+import re
+from copy import deepcopy
 import requests
 import FinanceDataReader as fdr
 import yfinance as yf
@@ -25,15 +27,38 @@ def get_json(url, keys=["result"]):
     return rst
 
 
-def fin_data(*arg, src="krx"):
-    if src == "yahoo":
-        d = yf.download(*arg)
+def fin_data(*arg, src="auto"):
+    arg_ = list( arg )
+    ticker = arg_[0]
+    m_kr = re.match(r"\d{6,6}", ticker )
+    m_us = re.match(r"[a-zA-Z]+", ticker )
+    
+    if m_kr is not None and m_kr[0] == ticker:
+        ticker_type = "KR"
+    elif m_us is not None and m_us[0] == "ticker":
+        ticker_type = "US"
     else:
-        d = fdr.DataReader(*arg)
+        ticker_type = None
+
+    if src == "krx":
+        d = fdr.DataReader( *arg )
+    elif src == "yahoo":
+        d = yf.download( *arg )
+    elif src == "auto":
+        if ticker_type == "KR":   # 한국 숫자 6자리 티커
+            arg_[0] = ticker + ".KS"
+            d = yf.download( *arg_ )
+            if len(d) < 1:  # yahoo finance에 데이터가 없다면
+                print( "There is no data in yahoo finance. Try KRX data")
+                d = fdr.DataReader( *arg )
+        else:
+            d = yf.download( *arg )
+        
     if 'Adj Close' in d.columns:
         print("Catching 'Adj Close'")
         rst = d['Adj Close']
     else:
         print("Catching 'Close'")
         rst = d['Close']
+        
     return rst
