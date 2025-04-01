2076a800dad7532d69e92e2abea5e221f7ab2c60
Junho <pinedance@gmail.com>
2025-04-01 17:51:02 +0900

    update

--- run_get_yh_etf_price.py
+++ run_get_yh_etf_price.py
@@ -5,7 +5,7 @@ from os import path
 import pandas as pd
 from tqdm import tqdm
 from tools import fin_data, get_price_status
-from message import notice_price_status
+from message import send_telegram_message, notice_price_status
 from cons import config_gsheet_tickers_req_yh as config_tickers_req
 from cons import delta_days
 
@@ -48,3 +48,6 @@ html_table = etf_data.to_html(na_rep='')
 rst_path = path.join("dist", "YH", "etf-adjusted-price-selected.html")
 with open(rst_path, "w", encoding="utf-8") as fl:
     fl.write(html_table)
+
+# %%
+send_telegram_message("YAHOO ETF PRICE 업데이트가 완료되었습니다.")
