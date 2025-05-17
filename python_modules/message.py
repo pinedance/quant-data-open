2cf18654e0ba1fc07c3fa5b17a7f3562cef5bfdd
Junho <pinedance@gmail.com>
2025-05-17 16:39:40 +0900

    update ma200 cross message

--- python_modules/message.py
+++ python_modules/message.py
@@ -32,19 +32,27 @@ def notice_price_status(data, tickers=None):
     
     try:
         status = data.get('status', '')
-        if not status or "지속" in status:
+        sigma_max, sigma_min = 1.8, -1.8
+        sigma = data.get('sigma', 0)
+        if (not status or "지속" in status) and (sigma < sigma_max or sigma > sigma_min):
             return
             
         ticker = data.get('ticker', 'Unknown')
         current_price = data.get('current_price', 0)
         ma200 = data.get('ma200', 0)
         
+        message = f"[{ticker}] {status}"
+        # message = f"[{ticker}] {status}\n{name}\n현재 가격: {current_price:.2f}\n200일 MA: {ma200:.2f}"
+
         if tickers and f"A{ticker}" in tickers:
             name = tickers[f"A{ticker}"]
-            message = f"[{ticker}] {status}\n{name}\n현재 가격: {current_price:.2f}\n200일 MA: {ma200:.2f}"
-        else:
-            message = f"[{ticker}] {status}\n현재 가격: {current_price:.2f}\n200일 MA: {ma200:.2f}"
+            message += f"\n{name}"
         
+        if (sigma > sigma_max) or (sigma < sigma_min):
+            message += f"\nSIGMA: {sigma:.1f}"
+        else:
+            message += f"\nSMA200: {ma200:.2f}"
+
         send_telegram_message(message)
     except Exception as e:
         send_telegram_message(f"Error in notice_price_status for {data.get('ticker', 'Unknown')}: {str(e)}")
